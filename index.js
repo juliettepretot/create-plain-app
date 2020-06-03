c7b46f4db275e6ef5b1f1c02566395e13e465fd8
Juliette Pretot <j@jul.sh>
2020-06-03 10:15:57 +0100

    use inline css

--- index.js
+++ index.js
@@ -5,42 +5,43 @@ const {
   writeFile: writeFileWithCallback
 } = require('fs')
 
-const HTML = `<!DOCTYPE html>
+function generateHTML(name) {
+  return `<!DOCTYPE html>
 <html>
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>Plain App</title>
-    <link rel="stylesheet" href="main.css" />
+    <title>${name}</title>
+    <style>
+      body,
+      html {
+        display: flex;
+        width: 100%;
+        min-height: 100%;
+        margin: 0;
+        padding: 0;
+      }
+
+      body {
+        background-color: #fff8f8;
+        color: #111c40;
+        font-size: 20px;
+        font-family: -apple-system, Roboto, Helvetica Neue, sans-serif;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+      }
+    </style>
   </head>
 
   <body>
     <h1>👩‍🏭 Welcome! ✨</h1>
+    <script type="module">
+      // ES Modules, ready to go. :)
+    </script>
   </body>
-
-  <script type="module">
-    // ES Modules, ready to go. :)
-  </script>
 </html>`
-
-const CSS = `body,
-html {
-  display: flex;
-  width: 100%;
-  min-height: 100%;
-  margin: 0;
-  padding: 0;
 }
-
-body {
-  background-color: #fff8f8;
-  color: #111c40;
-  font-size: 20px;
-  font-family: -apple-system, Roboto, Helvetica Neue, sans-serif;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-}`
 
 function generatePackageDotJson(name) {
   return `{
@@ -63,14 +64,14 @@ function generateSuccessMessage(name) {
 
 async function mkdir(path, options = {}) {
   await new Promise((resolve, reject) =>
-    mkdirWithCallback(path, options, error =>
+    mkdirWithCallback(path, options, (error) =>
       error ? reject(error) : resolve()
     )
   )
 }
 async function writeFile(file, data, options = {}) {
   await new Promise((resolve, reject) =>
-    writeFileWithCallback(file, data, options, error =>
+    writeFileWithCallback(file, data, options, (error) =>
       error ? reject(error) : resolve()
     )
   )
@@ -82,8 +83,7 @@ async function createPlainApp(name) {
   try {
     await mkdir(projectDir)
     await Promise.all([
-      writeFile(`${projectDir}/index.html`, HTML),
-      writeFile(`${projectDir}/main.css`, CSS),
+      writeFile(`${projectDir}/index.html`, generateHTML(name)),
       writeFile(`${projectDir}/package.json`, generatePackageDotJson(name))
     ])
     console.log(generateSuccessMessage(name))
